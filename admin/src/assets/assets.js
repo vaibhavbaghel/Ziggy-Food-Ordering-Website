2c5f725e6d2eb303c0b506a985272ce8876954f8
vaibhavbaghel <vaibhavbaghel0501@gmail.com>
2025-11-10 18:52:28 +0530

    changed backend url

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://food-delivery-backend-5b6g.onrender.com'
+export const url = 'https://ziggy-food-ordering-website-backend.onrender.com'
