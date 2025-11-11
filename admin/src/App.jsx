8c74540a9f5aeea49088078d357651524d7b6696
Vaibhav Baghel <149092281+vaibhavbaghel@users.noreply.github.com>
2025-11-11 10:52:10 +0530

    Update API URL to production endpoint

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import "react-toastify/dist/ReactToastify.css";
 import Login from "./components/Login/Login";
 
 const App = () => {
-  const url = "http://localhost:4000";
+  const url = "https://ziggy-food-ordering-website-backend.onrender.com";
   return (
     <div>
       <ToastContainer />
